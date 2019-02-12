4f2683138d4eee32749fb627c8d959646bb244e9
Daniel Ors <do7811@gmail.com>
2019-02-12 10:54:38 -0500

    starting point

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import './dummy-store'
 import './App.css';
 
 
 class App extends Component {
+  state = {
+    notes: [],
+    folders: [],
+  }
 
+  //render sidebar routes
+  renderSidebar() {
+    const { notes, folders } = this.state
+    return (
+        <>
+            <Route path='/' component={MainSidebar} />
+            <Route path='/foo' component={FooSidebar} />
+            <Route path='/' component={MainMain} />
+            <Route path='/foo' component={FooMain} />
+        </>
+    );
+  }
+  // then render the main routes
+  renderMain() {
+    const { notes, folders } = this.state
+    return (
+        <>
+            <Route path='/' component={MainSidebar} />
+            <Route path='/foo' component={FooSidebar} />
+            <Route path='/' component={MainMain} />
+            <Route path='/foo' component={FooMain} />
+        </>
+    );
+  }
+
+  // then render everything
   render() {
     
     return (
-      <main className='App'>
-        <Route />
-        <Route />
-        <Route />
-      </main>
+        <div className="app">
+          <nav>
+            {this.renderSidebar()}
+          </nav>
+          <header>
+            <h1>
+              <Link to='/'>Noteful</Link>
+              </h1>
+          </header>
+          <main>
+            {this.renderMain()}
+          </main>
+        </div>
     );
   }
 }
