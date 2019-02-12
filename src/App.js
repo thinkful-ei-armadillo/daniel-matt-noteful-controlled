7db8a0c028eea694ee9234b5b66668974a897f31
Daniel Ors <do7811@gmail.com>
2019-02-12 10:19:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+
+
+class App extends Component {
+
+  render() {
+    
+    return (
+      <main className='App'>
+        
+      </main>
+    );
+  }
+}
+
+export default App;
